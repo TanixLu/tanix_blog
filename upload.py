da309d8d56670e3a60ac6562c973f706884d0f62
Tanix <tanixlu@qq.com>
2026-06-24 09:51:21 +0800

    fix zip error

--- upload.py
+++ upload.py
@@ -1,3 +1,5 @@
+import time
+import struct
 import stat
 from typing import List
 from pathlib import Path
@@ -16,27 +18,69 @@ access_key_id = str(os.getenv("ALIYUN_ACCESS_KEY_ID"))
 access_key_secret = str(os.getenv("ALIYUN_ACCESS_KEY_SECRET"))
 
 
-def _add_file(zip_file: zipfile.ZipFile, file: Path, arcname: str):
-    zinfo = zipfile.ZipInfo(arcname.replace("\\", "/"))
-    zinfo.external_attr = (stat.S_IFDIR | 0o755) << 16 | 0x10
-    zinfo.create_system = 3  # Unix
-    zip_file.writestr(zinfo, file.read_bytes())
+def _make_extra_field(mtime: float) -> bytes:
+    """生成 UT extra field (0x5455)，与 Linux zip 一致。"""
+    # flag=0x01 表示只含 mtime
+    return struct.pack("<HHB I", 0x5455, 5, 0x01, int(mtime))
+
+
+def _add_dir(zf: zipfile.ZipFile, arcname: str, mtime: float):
+    dirpath = arcname.replace("\\", "/")
+    if not dirpath.endswith("/"):
+        dirpath += "/"
+
+    info = zipfile.ZipInfo(dirpath)
+    info.create_system = 3
+    info.create_version = 30
+    info.extract_version = 10
+    info.compress_type = zipfile.ZIP_STORED
+    info.external_attr = (stat.S_IFDIR | 0o755) << 16
+    info.date_time = time.localtime(mtime)[:6]
+    info.extra = _make_extra_field(mtime)
+    zf.writestr(info, b"")
+
+
+def _add_file(zf: zipfile.ZipFile, file: Path, arcname: str):
+    data = file.read_bytes()
+    mtime = file.stat().st_mtime
+
+    info = zipfile.ZipInfo(arcname.replace("\\", "/"))
+    info.create_system = 3  # Unix
+    info.create_version = 30  # 3.0
+    info.extract_version = 10  # 1.0 (store 只需 1.0)
+    info.compress_type = zipfile.ZIP_STORED
+    info.external_attr = (stat.S_IFREG | 0o755) << 16  # 普通文件 + 755
+    info.date_time = time.localtime(mtime)[:6]
+    info.extra = _make_extra_field(mtime)
+    zf.writestr(info, data)
 
 
 def zip2base64(path_list: List[Path]) -> str:
     zip_buffer = BytesIO()
-    with zipfile.ZipFile(zip_buffer, "w", zipfile.ZIP_DEFLATED) as zip_file:
+    added_dirs: set[str] = set()
+
+    with zipfile.ZipFile(zip_buffer, "w", zipfile.ZIP_STORED) as zf:
         for path in path_list:
             if path.is_dir():
                 for file in path.rglob("*"):
-                    if file.is_file():
-                        _add_file(zip_file, file, str(file.relative_to(path.parent)))
+                    arcname = str(file.relative_to(path.parent))
+                    if file.is_dir():
+                        if arcname not in added_dirs:
+                            added_dirs.add(arcname)
+                            _add_dir(zf, arcname, file.stat().st_mtime)
+                    elif file.is_file():
+                        # 补齐父目录
+                        parts = arcname.replace("\\", "/").split("/")
+                        for i in range(1, len(parts)):
+                            d = "/".join(parts[:i])
+                            if d not in added_dirs:
+                                added_dirs.add(d)
+                                _add_dir(zf, d, file.parent.stat().st_mtime)
+                        _add_file(zf, file, arcname)
             else:
-                _add_file(zip_file, path, path.name)
+                _add_file(zf, path, path.name)
 
-    zip_bytes = zip_buffer.getvalue()
-
-    return b64encode(zip_bytes).decode()
+    return b64encode(zip_buffer.getvalue()).decode()
 
 
 def fc_upload(path_list: List[Path], region: str, function_name: str) -> None:
